d3ef991b6c630872abf02da98509f5f890b1b430
emelieskepo <skepo@student.chalmers.se>
2023-12-06 11:12:46 +0100

    6 dec

--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -3,7 +3,6 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.util.ArrayList;
 import java.util.HashMap;
 // This panel represent the animated part of the view with the car images.
 
